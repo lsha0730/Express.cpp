09216a7109aa30614ab40c9b57e0010db6352ef7
Lincoln Lee <Lsha0730@gmail.com>
2025-04-07 23:31:32 -0700

    refactor: make UrlCodec methods static

--- src/http/url_codec.h
+++ src/http/url_codec.h
@@ -8,8 +8,8 @@
 namespace flash {
 class UrlCodec {
 public:
-  std::string decode(std::string_view);
-  //   std::string encode(std::string_view); TODO
+  static std::string decode(std::string_view);
+  //   static std::string encode(std::string_view); TODO
 
 private:
   struct utf8_decoded {
@@ -17,10 +17,10 @@ private:
     int new_index;
   };
 
-  utf8_decoded decode_percent_encoding(std::string_view str, int i);
-  int get_utf8_sequence_length(unsigned char first_byte);
-  std::string hex_to_utf8(const std::string &hex_str);
-  bool is_valid_hex_digit(char);
+  static utf8_decoded decode_percent_encoding(std::string_view str, int i);
+  static int get_utf8_sequence_length(unsigned char first_byte);
+  static std::string hex_to_utf8(const std::string &hex_str);
+  static bool is_valid_hex_digit(char);
 };
 } // namespace flash
 
