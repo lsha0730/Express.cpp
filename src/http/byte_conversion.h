e68e23a840014d4f8878dca4a8c213cfbce72d0a
Lincoln Lee <Lsha0730@gmail.com>
2025-04-24 01:38:34 -0700

    chore: improve byte conversion logic

--- src/http/byte_conversion.h
+++ src/http/byte_conversion.h
@@ -2,6 +2,8 @@
 #define FLASH_BYTE_CONVERSION
 
 #include "include/flash/concepts.h"
+#include <filesystem>
+#include <iterator>
 
 namespace flash {
 template <BufferLike T> std::vector<char> to_bytes(const T &input) {
@@ -9,10 +11,22 @@ template <BufferLike T> std::vector<char> to_bytes(const T &input) {
   return std::vector<char>(data_ptr, data_ptr + std::size(input));
 }
 
+template <typename T, size_t N> std::vector<char> to_bytes(const T (&input)[N]) {
+  return std::vector<char>(input, input + N);
+}
+
 template <StringLike T> std::vector<char> to_bytes(const T &input) {
   std::vector<char> data;
-  data.reserve(input.size());
-  data.insert(data.end(), input.begin(), input.end());
+  if constexpr (std::is_same_v<std::decay_t<T>, char>) {
+    data.push_back(input);
+  } else if constexpr (std::is_same_v<std::decay_t<T>, std::filesystem::path>) {
+    std::string stringified(input);
+    data.reserve(stringified.size());
+    data.insert(data.end(), stringified.begin(), stringified.end());
+  } else {
+    data.reserve(input.size());
+    data.insert(data.end(), input.begin(), input.end());
+  }
   return data;
 }
 } // namespace flash
